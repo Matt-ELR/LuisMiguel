85f219e12fe1375995e9dfdc67fe669e261d5ac8
Matt-ELR <131918424+Matt-ELR@users.noreply.github.com>
2024-10-09 15:04:07 -0600

    Pre-Control

--- IMdV/control.php
+++ IMdV/control.php
@@ -27,7 +27,48 @@
     
 
     <main>
-        
+    <?php include 'php/conexion.php'; ?> <!-- Conecta a la base de datos -->
+    <table class="tablaprofes">
+        <th>(imagen)</th>
+        <th>Nombre</th>
+        <th>Estudios</th>
+        <th></th>
+
+        <?php
+        $selec=$con->query("SELECT * FROM profesores"); //Consigue la informacion de la tabla "Profesores"
+        while ($fila = $selec -> fetch_assoc()){?> <!--Mientras haya mas filas, repetira las siguientes lineas-->
+        <tr>
+          <td> <?php echo $fila['Apellido']?></td> <!-- Escribe la entrada N de la fila Apellido -->
+          <td> <?php echo $fila['Nombre']?></td>   <!-- Escribe la entrada N de la fila Nombre -->
+          <td> <?php echo $fila['Estudios']?></td> <!-- Escribe la entrada N de la fila Estudios -->
+          <td> <a href ="profesores2.php?ID=<?php echo $fila['ID'] ?>">Ver informacion</a></td> <!-- Añade un enlaze para
+            ver mas detalles de la entrada en cuestion -->
+          <td><a href ="php/borrar.php?ID=<?php echo $fila['ID'] ?>">Borrar</a></td>
+        </tr>
+        <?php } ?> <!--Cierra el bucle while -->
+      </table>
+    <form action="php/nuevo.php" method="post">
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" id="nombre" name="Nombre" placeholder="Nombre" required>
+        <br><br>
+        <label for="apellido">Apellido:</label><br>
+        <input type="text" id="apellido" name="Apellido" placeholder="Apellido" required>
+        <br><br>
+        <label for="estudios">Estudios:</label><br>
+        <input type="text" id="estudios" name="Estudios" placeholder="Estudios" required>
+        <br><br>
+        <label for="informacion">Ingrese informacion de el profesor:</label><br>
+        <textarea name="Informacion" id="informacion" maxlength="255" required placeholder="informacion"></textarea>
+        <br><br>
+        <label for="foto">Suba la foto del profesor</label>
+        <input type="file" id="foto" name="foto" required >
+        <br><br>
+        <input type="submit" value="Subir informacion">
+        <input type="reset" value="Vaciar">
+        </div>
+  </form>
+
+
     </main>
     
     <footer class="secondary-footer">
---
+++ IMdV/php/borrar.php
@@ -0,0 +1,15 @@
+<?php
+include 'conexion.php';
+$ID = $_REQUEST['ID'];
+$del = $con -> query("DELETE FROM profesores WHERE ID = '$ID'");
+if ($del) {
+    echo "<script>
+    location.href='../control.php';
+    </script>";
+} else {
+    echo "<script>
+    alert('El registro no pudo ser eliminado');
+    location.href='../control.php';
+    </script>";
+}
+?>
